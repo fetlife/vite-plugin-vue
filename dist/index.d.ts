78aaaf9d9795c0c153b28bac6a7d1a2198987db4
Andrii Dmytrenko <andrii@fetlife.com>
2022-11-04 12:54:46 +0000

    Build for vite 3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,24 +1,30 @@
-import { Plugin as Plugin_2 } from 'vite';
-import { SFCScriptCompileOptions } from '@vue/compiler-sfc';
-import { SFCStyleCompileOptions } from '@vue/compiler-sfc';
-import { SFCTemplateCompileOptions } from '@vue/compiler-sfc';
-import { ViteDevServer } from 'vite';
+import { ViteDevServer, Plugin } from 'vite';
+import * as _compiler from 'vue/compiler-sfc';
+import { SFCScriptCompileOptions, SFCTemplateCompileOptions, SFCStyleCompileOptions } from 'vue/compiler-sfc';
 
-export declare interface Options {
+interface VueQuery {
+    vue?: boolean;
+    src?: string;
+    type?: 'script' | 'template' | 'style' | 'custom';
+    index?: number;
+    lang?: string;
+    raw?: boolean;
+    scoped?: boolean;
+}
+declare function parseVueRequest(id: string): {
+    filename: string;
+    query: VueQuery;
+};
+
+interface Options {
     include?: string | RegExp | (string | RegExp)[];
     exclude?: string | RegExp | (string | RegExp)[];
     isProduction?: boolean;
-    /**
-     * Enable or disable usage of inline template for the script setup
-     * @default true
-     */
-    allowUseInlineTemplate?: boolean;
-    script?: Partial<SFCScriptCompileOptions>;
-    template?: Partial<SFCTemplateCompileOptions>;
-    style?: Partial<SFCStyleCompileOptions>;
+    script?: Partial<Pick<SFCScriptCompileOptions, 'babelParserPlugins'>>;
+    template?: Partial<Pick<SFCTemplateCompileOptions, 'compiler' | 'compilerOptions' | 'preprocessOptions' | 'preprocessCustomRequire' | 'transformAssetUrls'>>;
+    style?: Partial<Pick<SFCStyleCompileOptions, 'trim'>>;
     /**
      * Transform Vue SFCs into custom elements.
-     * **requires Vue \>= 3.2.0 & Vite \>= 2.4.4**
      * - `true`: all `*.vue` imports are converted into custom elements
      * - `string | RegExp`: matched files are converted into custom elements
      *
@@ -26,47 +32,35 @@ export declare interface Options {
      */
     customElement?: boolean | string | RegExp | (string | RegExp)[];
     /**
-     * Enable Vue ref transform (experimental).
-     * https://github.com/vuejs/vue-next/tree/master/packages/ref-transform
-     *
-     * **requires Vue \>= 3.2.5**
-     *
+     * Enable Vue reactivity transform (experimental).
+     * https://vuejs.org/guide/extras/reactivity-transform.html
      * - `true`: transform will be enabled for all vue,js(x),ts(x) files except
      *           those inside node_modules
      * - `string | RegExp`: apply to vue + only matched files (will include
-     *                      node_modules, so specify directories in necessary)
+     *                      node_modules, so specify directories if necessary)
      * - `false`: disable in all cases
      *
      * @default false
      */
-    refTransform?: boolean | string | RegExp | (string | RegExp)[];
+    reactivityTransform?: boolean | string | RegExp | (string | RegExp)[];
     /**
-     * @deprecated the plugin now auto-detects whether it's being invoked for ssr.
+     * Use custom compiler-sfc instance. Can be used to force a specific version.
      */
-    ssr?: boolean;
+    compiler?: typeof _compiler;
+    /**
+     * Enable or disable usage of inline template for the script setup
+     * @default true
+     */
+    allowUseInlineTemplate?: boolean;
 }
-
-export declare function parseVueRequest(id: string): {
-    filename: string;
-    query: VueQuery;
-};
-
-export declare interface ResolvedOptions extends Options {
+interface ResolvedOptions extends Options {
+    compiler: typeof _compiler;
     root: string;
     sourceMap: boolean;
+    cssDevSourcemap: boolean;
     devServer?: ViteDevServer;
+    devToolsEnabled?: boolean;
 }
+declare function vuePlugin(rawOptions?: Options): Plugin;
 
-declare function vuePlugin(rawOptions?: Options): Plugin_2;
-export default vuePlugin;
-
-export declare interface VueQuery {
-    vue?: boolean;
-    src?: boolean;
-    type?: 'script' | 'template' | 'style' | 'custom';
-    index?: number;
-    lang?: string;
-    raw?: boolean;
-}
-
-export { }
+export { Options, ResolvedOptions, VueQuery, vuePlugin as default, parseVueRequest };
